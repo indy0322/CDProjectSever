05509488bdbe7d34d699a933d5f8df99f29f7dc2
indy0322 <indy032229@gmail.com>
2024-02-22 10:34:45 +0900

    commit

--- controller/api.js
+++ controller/api.js
@@ -118,7 +118,7 @@ const apiAudio = async (req, res) => {
     const mp3 = await openai.audio.speech.create({
         model: "tts-1",
         voice: "alloy",
-        input: "你好"
+        input: req.body.speak
     })
 
     const buffer = Buffer.from(await mp3.arrayBuffer())
